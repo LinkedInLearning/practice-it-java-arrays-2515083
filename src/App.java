f0c93665288c17e587d217b3210787f19117ebc8
Kathryn Hodge <blondiebytes@gmail.com>
2022-10-23 16:49:44 +0000

    Implement triangle function

--- src/App.java
+++ src/App.java
@@ -1,14 +1,23 @@
 public class App {
-  
+  // { A, B, C }
+  // A
+  // AB
+  // ABC
   public static void printTriangle(Object[] arr) {
-
- }
+    for (int row = 0; row < arr.length; row++) {
+      for (int col = 0; col <= row; col++) {
+        System.out.print(arr[col]);
+      }
+      System.out.println();
+    }
+    System.out.println();
+  }
     
-  public static void main(String args[]) {
-    printTriangle(new Integer[] {1, 2, 3, 4});
+  public static void main(String  args[]) { 
+    printTriangle(new Integer[] {1,  2, 3, 4}); 
     printTriangle(new Character[] {'A', 'B', 'C', 'D', 'E'});
-    printTriangle(new String[] {});
-    printTriangle(new String[] {"Hello"});
+    printTriangle(new String[] { }); 
+    printTriangle(new String[] { "Hello"}); 
     printTriangle(new String[] {"Hello", "There", "Friend"});
   }
 }
