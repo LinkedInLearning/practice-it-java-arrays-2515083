5081ac605b8ec19f6a02cb0a468da4e7d08e3d51
Kathryn Hodge <blondiebytes@gmail.com>
2022-10-23 14:05:16 +0000

    Re-init

--- src/App.java
+++ src/App.java
@@ -1,9 +1,6 @@
 public class App {
-  
+
   public static void main(String[] args) {
-    double[] lotteryNums = { 45, 92, 38, 33, 21 };
-    System.out.println(lotteryNums[2]);
-    lotteryNums[2] = 40;
-    System.out.println(lotteryNums[2]);
+
   }
 }
