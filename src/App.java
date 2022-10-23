4374491759a46313cb6c8c56f12813b0acc2950f
Kathryn Hodge <blondiebytes@gmail.com>
2022-10-23 16:18:17 +0000

    init challenge

--- src/App.java
+++ src/App.java
@@ -1,6 +1,14 @@
 public class App {
   
-  public static void main(String[] args) {
+  public static void printTriangle(Object[] arr) {
+
+ }
     
+  public static void main(String args[]) {
+    printTriangle(new Integer[] {1, 2, 3, 4});
+    printTriangle(new Character[] {'A', 'B', 'C', 'D', 'E'});
+    printTriangle(new String[] {});
+    printTriangle(new String[] {"Hello"});
+    printTriangle(new String[] {"Hello", "There", "Friend"});
   }
 }
