c9daa43200f9f158396582d60c6f3f816adacc48
Kathryn Hodge <blondiebytes@gmail.com>
2022-10-23 15:58:13 +0000

    Implement find second smallest item

--- src/App.java
+++ src/App.java
@@ -1,7 +1,40 @@
+import java.util.Arrays;
+
 public class App {
   
+  public static Integer findSecondSmallestItem2(Integer[] arr) {
+    if (arr.length <= 1) {
+      return null;
+    }
+    Arrays.sort(arr);
+    for (int i = 0; i < arr.length - 1; i++) {
+      if (arr[i] != arr[i+1]) {
+        return arr[i+1];
+      }
+    }
+    return null;
+    // {3, 3, 3, 3, 3, 3}
+  }
+
   public static Integer findSecondSmallestItem(Integer[] arr) {
-    return 0;
+    int smallest = Integer.MAX_VALUE;
+    int secondSmallest = Integer.MAX_VALUE;
+
+    for (int i = 0; i < arr.length; i++) {
+      int current = arr[i];
+      if (current < smallest) {
+        secondSmallest = smallest;
+        smallest = current;
+      } else if (current < secondSmallest && current != smallest) {
+        secondSmallest = current;
+      }
+    }
+
+    if (secondSmallest == Integer.MAX_VALUE) {
+      return null;
+    }
+
+    return secondSmallest;
   }
     
   public static void main(String args[]) {
