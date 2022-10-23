5052fecbffb86b3a973dd42f31f471beda3a0687
Kathryn Hodge <blondiebytes@gmail.com>
2022-10-23 15:14:42 +0000

    init challenge

--- src/App.java
+++ src/App.java
@@ -1,6 +1,32 @@
 public class App {
   
-  public static void main(String[] args) {
-    
+  public static Integer findSecondSmallestItem(Integer[] arr) {
+    return 0;
   }
+    
+  public static void main(String args[]) {
+    Integer[] arr = new Integer[]{ 5, 8, 3, 2, 6 };
+    System.out.println(findSecondSmallestItem(arr));
+    
+    Integer[] arr2 = new Integer[]{ 3, 8, 5, 2, 6 };
+    System.out.println(findSecondSmallestItem(arr2));
+    
+    Integer[] arr3 = new Integer[]{ 6, 8, 5, 2, 3 };
+    System.out.println(findSecondSmallestItem(arr3));
+    
+    Integer[] arr4 = new Integer[]{ 3, 3, 3, 3, 3 };
+    System.out.println(findSecondSmallestItem(arr4));
+    
+    Integer[] arr5 = new Integer[]{ 3, 3, 3, 2, 3 };
+    System.out.println(findSecondSmallestItem(arr5));
+    
+    Integer[] arr6 = new Integer[]{ 3, 4, 3, 3, 3 };
+    System.out.println(findSecondSmallestItem(arr6));
+    
+    Integer[] arrEmpty = new Integer[] {};
+    System.out.println(findSecondSmallestItem(arrEmpty));
+    
+    Integer[] arrOne = new Integer[] { 1 };
+    System.out.println(findSecondSmallestItem(arrOne));
+    }
 }
