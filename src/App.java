ff40f6c5dba079d24ede4fb299ee53726c97d5e7
Kathryn Hodge <blondiebytes@gmail.com>
2022-10-25 12:05:51 +0000

    init challenge

--- src/App.java
+++ src/App.java
@@ -1,6 +1,30 @@
 public class App {
   
-  public static void main(String[] args) {
+  public static Object[] rotateRight(Object[] arr) {
+	  return arr;
+  }
+  
+  public static void main(String args[]) {
+    Object[] arr = new Integer[] {1, 2, 3, 4, 5};
+      
+    arr = rotateRight(arr);
+    System.out.println(Arrays.toString(arr));
+    arr = rotateRight(arr);
+    System.out.println(Arrays.toString(arr));
+    arr = rotateRight(arr);
+    System.out.println(Arrays.toString(arr));
+    arr = rotateRight(arr);
+    System.out.println(Arrays.toString(arr));
+    arr = rotateRight(arr);
+    System.out.println(Arrays.toString(arr));
     
+    Object[] arr2 = new String[] {"Hello", "there", "my friend"};
+    
+    arr2 = rotateRight(arr2);
+    System.out.println(Arrays.toString(arr2));
+    arr2 = rotateRight(arr2);
+    System.out.println(Arrays.toString(arr2));
+    arr2 = rotateRight(arr2);
+    System.out.println(Arrays.toString(arr2));
   }
 }
