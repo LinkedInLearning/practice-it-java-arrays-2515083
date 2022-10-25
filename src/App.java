c46d721c9664c4fdb70e93ae38887656580f1896
Kathryn Hodge <blondiebytes@gmail.com>
2022-10-25 12:45:36 +0000

    init challenge

--- src/App.java
+++ src/App.java
@@ -1,6 +1,52 @@
+import java.util.Arrays;
+
 public class App {
-  
-  public static void main(String[] args) {
+
+  public static void moveZerosToTheEnd(int[] arr) {
     
+  }
+
+  public static void main(String[] args) {
+    int[] arr = new int[] {1, 1, 0, 0, 0, 1, 0};
+    System.out.println(Arrays.toString(arr));
+    moveZerosToTheEnd(arr);
+    System.out.println(Arrays.toString(arr));
+    System.out.println();
+
+    int[] arr2 = new int[] {0, 0, 1, 2, 3, 0, 4};
+    System.out.println(Arrays.toString(arr2));
+    moveZerosToTheEnd(arr2);
+    System.out.println(Arrays.toString(arr2));
+    System.out.println();
+
+    int[] arr3 = new int[] {0, 0};
+    System.out.println(Arrays.toString(arr3));
+    moveZerosToTheEnd(arr3);
+    System.out.println(Arrays.toString(arr3));
+    System.out.println();
+
+    int[] arr4 = new int[] {2, 9, 1};
+    System.out.println(Arrays.toString(arr4));
+    moveZerosToTheEnd(arr4);
+    System.out.println(Arrays.toString(arr4));
+    System.out.println();
+
+    int[] arr5 = new int[] {0, 10, 0};
+    System.out.println(Arrays.toString(arr5));
+    moveZerosToTheEnd(arr5);
+    System.out.println(Arrays.toString(arr5));
+    System.out.println();
+
+    int[] arr6 = new int[] {3, 0, 0};
+    System.out.println(Arrays.toString(arr6));
+    moveZerosToTheEnd(arr6);
+    System.out.println(Arrays.toString(arr6));
+    System.out.println();
+
+    int[] arr7 = new int[] {};
+    System.out.println(Arrays.toString(arr7));
+    moveZerosToTheEnd(arr7);
+    System.out.println(Arrays.toString(arr7));
+    System.out.println();
   }
 }
