277a72395b4b7f3c8e659476c818393b27f49b45
Kathryn Hodge <blondiebytes@gmail.com>
2022-10-25 12:22:06 +0000

    Undo implementation

--- src/App.java
+++ src/App.java
@@ -3,18 +3,7 @@ import java.util.Arrays;
 public class App {
 
   public static Object[] rotateRight(Object[] arr) {
-    if (arr == null) {
-      return null;
-    }
-
-    Object[] result = new Object[arr.length];
-    for (int i = 0; i < arr.length; i++) {
-      result[(i + 1) % result.length] = arr[i];
-    }
-    // {1, 2, 3, 4, 5}
-    // {null, 1, 2, 3, 4} 5?
-    // {5, 1, 2, 3, 4}
-    return result;
+    return arr;
   }
 
   public static void main(String args[]) {
