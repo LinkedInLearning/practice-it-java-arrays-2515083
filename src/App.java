9e3dd4288d14922da7925b4ed0647549d618ee99
Kathryn Hodge <blondiebytes@gmail.com>
2022-10-25 12:31:16 +0000

    Implement rightRotation

--- src/App.java
+++ src/App.java
@@ -3,7 +3,17 @@ import java.util.Arrays;
 public class App {
 
   public static Object[] rotateRight(Object[] arr) {
-    return arr;
+    if (arr == null) {
+      return null;
+    }
+    Object[] result = new Object[arr.length];
+    // Result[] -> {null, null, null, null, null}
+    for (int i = 0; i < arr.length; i++) {
+      // arr[] --> {1, 2, 3, 4, 5}
+      // result[] -> {5, 1, 2, 3, 4}
+      result[(i+1) % result.length] = arr[i];
+    }
+    return result;
   }
 
   public static void main(String args[]) {
