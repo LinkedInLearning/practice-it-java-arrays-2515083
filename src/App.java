c512e3950f9e4f415ff92bee5313a710d4b0ede2
Kathryn Hodge <blondiebytes@gmail.com>
2022-10-25 12:20:59 +0000

    implement rotate array

--- src/App.java
+++ src/App.java
@@ -1,12 +1,25 @@
+import java.util.Arrays;
+
 public class App {
-  
+
   public static Object[] rotateRight(Object[] arr) {
-	  return arr;
+    if (arr == null) {
+      return null;
+    }
+
+    Object[] result = new Object[arr.length];
+    for (int i = 0; i < arr.length; i++) {
+      result[(i + 1) % result.length] = arr[i];
+    }
+    // {1, 2, 3, 4, 5}
+    // {null, 1, 2, 3, 4} 5?
+    // {5, 1, 2, 3, 4}
+    return result;
   }
-  
+
   public static void main(String args[]) {
-    Object[] arr = new Integer[] {1, 2, 3, 4, 5};
-      
+    Object[] arr = new Integer[] { 1, 2, 3, 4, 5 };
+
     arr = rotateRight(arr);
     System.out.println(Arrays.toString(arr));
     arr = rotateRight(arr);
@@ -17,9 +30,9 @@ public class App {
     System.out.println(Arrays.toString(arr));
     arr = rotateRight(arr);
     System.out.println(Arrays.toString(arr));
-    
-    Object[] arr2 = new String[] {"Hello", "there", "my friend"};
-    
+
+    Object[] arr2 = new String[] { "Hello", "there", "my friend" };
+
     arr2 = rotateRight(arr2);
     System.out.println(Arrays.toString(arr2));
     arr2 = rotateRight(arr2);
