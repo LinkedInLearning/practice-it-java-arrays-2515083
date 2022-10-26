3211603e6f40a571feabfcd4f01c0196842d86ab
Kathryn Hodge <blondiebytes@gmail.com>
2022-10-26 12:31:13 +0000

    Init challenge

--- src/App.java
+++ src/App.java
@@ -1,6 +1,46 @@
+import java.util.Arrays;
+
 public class App {
-  
-  public static void main(String[] args) {
     
+  public static void reverse(int[] arr) {
+    
+  }
+  
+  public static void main(String args[]) {
+    int[] arr = new int[] {};
+    System.out.println(Arrays.toString(arr));
+    reverse(arr);
+    System.out.println(Arrays.toString(arr));
+    System.out.println();
+    
+    int[] arr2 = new int[] {1};
+    System.out.println(Arrays.toString(arr2));
+    reverse(arr2);
+    System.out.println(Arrays.toString(arr2));
+    System.out.println();
+    
+    int[] arr3 = new int[] {1, 2, 3};
+    System.out.println(Arrays.toString(arr3));
+    reverse(arr3);
+    System.out.println(Arrays.toString(arr3));
+    System.out.println();
+    
+    int[] arr4 = new int[] {1, 2, 3, 4, 5};
+    System.out.println(Arrays.toString(arr4));
+    reverse(arr4);
+    System.out.println(Arrays.toString(arr4));
+    System.out.println();
+
+    int[] arr5 = new int[] {9, 3, 4, 1};
+    System.out.println(Arrays.toString(arr5));
+    reverse(arr5);
+    System.out.println(Arrays.toString(arr5));
+    System.out.println();
+    
+    int[] arr6 = new int[] {1, 2, 3, 4, 5, 6, 7};
+    System.out.println(Arrays.toString(arr6));
+    reverse(arr6);
+    System.out.println(Arrays.toString(arr6));
+    System.out.println();
   }
 }
