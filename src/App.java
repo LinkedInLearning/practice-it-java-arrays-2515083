11da98f9bbe60d4300852aacdc1e67b188280ed6
Kathryn Hodge <blondiebytes@gmail.com>
2022-10-26 12:26:53 +0000

    Implement moveAllZerosToEnd

--- src/App.java
+++ src/App.java
@@ -3,41 +3,52 @@ import java.util.Arrays;
 public class App {
 
   public static void moveZerosToTheEnd(int[] arr) {
-    
+    int count = 0;
+    // {4, 2, 0, 2}
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] != 0) {
+        arr[count] = arr[i];
+        count++;
+      }
+    }
+    while (count < arr.length) {
+      arr[count] = 0;
+      count++;
+    }
   }
 
   public static void main(String[] args) {
-    int[] arr = new int[] {1, 1, 0, 0, 0, 1, 0};
+    int[] arr = new int[] { 1, 1, 0, 0, 0, 1, 0 };
     System.out.println(Arrays.toString(arr));
     moveZerosToTheEnd(arr);
     System.out.println(Arrays.toString(arr));
     System.out.println();
 
-    int[] arr2 = new int[] {0, 0, 1, 2, 3, 0, 4};
+    int[] arr2 = new int[] { 0, 0, 1, 2, 3, 0, 4 };
     System.out.println(Arrays.toString(arr2));
     moveZerosToTheEnd(arr2);
     System.out.println(Arrays.toString(arr2));
     System.out.println();
 
-    int[] arr3 = new int[] {0, 0};
+    int[] arr3 = new int[] { 0, 0 };
     System.out.println(Arrays.toString(arr3));
     moveZerosToTheEnd(arr3);
     System.out.println(Arrays.toString(arr3));
     System.out.println();
 
-    int[] arr4 = new int[] {2, 9, 1};
+    int[] arr4 = new int[] { 2, 9, 1 };
     System.out.println(Arrays.toString(arr4));
     moveZerosToTheEnd(arr4);
     System.out.println(Arrays.toString(arr4));
     System.out.println();
 
-    int[] arr5 = new int[] {0, 10, 0};
+    int[] arr5 = new int[] { 0, 10, 0 };
     System.out.println(Arrays.toString(arr5));
     moveZerosToTheEnd(arr5);
     System.out.println(Arrays.toString(arr5));
     System.out.println();
 
-    int[] arr6 = new int[] {3, 0, 0};
+    int[] arr6 = new int[] { 3, 0, 0 };
     System.out.println(Arrays.toString(arr6));
     moveZerosToTheEnd(arr6);
     System.out.println(Arrays.toString(arr6));
