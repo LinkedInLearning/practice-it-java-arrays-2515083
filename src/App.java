93ca4927f3d8158d48440140aa070d1ab5256b24
Kathryn Hodge <blondiebytes@gmail.com>
2022-10-26 13:00:25 +0000

    init challenge

--- src/App.java
+++ src/App.java
@@ -2,5 +2,40 @@ public class App {
   
   public static void main(String[] args) {
     
+    CustomArrayList<Integer> customArrayList = new CustomArrayList<>();
+    System.out.println(customArrayList.size());
+
+    customArrayList.add(1);
+    System.out.println(customArrayList.get(0));
+    System.out.println(customArrayList.size());
+
+    customArrayList.add(2);
+    System.out.println(customArrayList.get(1));
+    System.out.println(customArrayList.size());
+
+    customArrayList.add(3);
+    System.out.println(customArrayList.get(2));
+    System.out.println(customArrayList.size());
+
+    System.out.println();
+
+    CustomArrayList<String> customArrayList2 = new CustomArrayList<>();
+    System.out.println(customArrayList2.size());
+    
+    customArrayList2.add("Hi");
+    System.out.println(customArrayList2.get(0));
+    System.out.println(customArrayList2.size());
+
+    customArrayList2.add("Hello");
+    System.out.println(customArrayList2.get(1));
+    System.out.println(customArrayList2.size());
+
+    customArrayList2.add("Hey");
+    System.out.println(customArrayList2.get(2));
+    System.out.println(customArrayList2.size());
+
+    customArrayList2.add("Good morning");
+    System.out.println(customArrayList2.get(3));
+    System.out.println(customArrayList2.size());
   }
 }
---
+++ src/CustomArrayList.java
@@ -0,0 +1,23 @@
+public class CustomArrayList<D> {
+  
+  private int size = 0;
+  private static final int DEFAULT_CAPACITY = 2;
+  private Object elements[];
+
+  public CustomArrayList() {
+    elements = new Object[DEFAULT_CAPACITY];
+  }
+
+  public D get(int i) {
+      return null;
+  }
+
+  public void add(D item) {
+
+  }
+
+  public int size() {
+      return size;
+  }
+
+}
